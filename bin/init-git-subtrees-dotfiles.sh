bfaffc434a6675f29ced5744039191949f7aefe9
Antonin Fischer <tonda.fischer@gmail.com>
2021-08-30 22:19:50 +0200

    Doom Emacs Configuration added.

--- bin/init-git-subtrees-dotfiles.sh
+++ bin/init-git-subtrees-dotfiles.sh
@@ -5,3 +5,4 @@ function config {
 
 config subtree add --prefix .config/awesome git@github.com:raven2cz/awesomewm-config.git master --squash
 config subtree add --prefix .config/polybar git@github.com:raven2cz/polybar-config.git main --squash
+config subtree add --prefix .config/doom git@github.com:raven2cz/emacs.git main --squash
