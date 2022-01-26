df6be3289ad4c9dddf44b8f765963a5428e534df
Antonin Fischer <tonda.fischer@gmail.com>
2022-01-26 09:58:50 +0100

    Upgrade configuration of kitty and alacritty to new versions.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -59,7 +59,7 @@
    ;; Global settings (defaults)
    (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
          doom-themes-enable-italic t) ; if nil, italics is universally disabled
-(load-theme 'doom-dracula t)
+(load-theme 'doom-gruvbox t)
 
    ;; Enable flashing mode-line on errors
    (doom-themes-visual-bell-config)
