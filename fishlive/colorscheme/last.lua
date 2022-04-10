a55f77f581aa435bca54f21213a184f07df3c879
Antonin Fischer <tonda.fischer@gmail.com>
2022-04-10 19:57:51 +0200

    Squashed '.config/awesome/' changes from 2d24988..c129c3b
    c129c3b Update Readme.
    8e97bba Update Readme.
    583a60a Image Collage Feature added.
    
    git-subtree-dir: .config/awesome
    git-subtree-split: c129c3b079b7e0d05ff268ed18f482b53a9e3ab9

---
+++ fishlive/colorscheme/last.lua
@@ -0,0 +1,1 @@
+return require "fishlive.colorscheme".doom_one
---
+++ fishlive/init.lua
@@ -0,0 +1,20 @@
+--[[
+
+Fishlive Lua Library
+Layouts, widgets and utilities for Awesome WM
+
+Main package
+
+Licensed under GNU General Public License v2
+ * (c) 2022, A.Fischer
+
+--]]
+
+return {
+    layout = require("fishlive.layout"),
+    util = require("fishlive.util"),
+    widget = require("fishlive.widget"),
+    wallpaper = require("fishlive.wallpaper"),
+    colorscheme = require("fishlive.colorscheme"),
+    collage = require("fishlive.collage")
+}
