6785a3cb50bbfcceefec682e73e5c6b279a9cc95
Antonin Fischer <tonda.fischer@gmail.com>
2022-06-04 22:06:22 +0200

    Squashed '.config/nvim/' changes from 11aaddbe..48f6ee81
    48f6ee81 Move icons global var to setup icons.glyphs.
    
    git-subtree-dir: .config/nvim
    git-subtree-split: 48f6ee819595840471d80fb71770e2ade3013874

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,25 +1,5 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
@@ -74,6 +54,26 @@ nvim_tree.setup {
     },
     icons = {
       webdev_colors = true,
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+      }
     },
   },
   hijack_directories = {
