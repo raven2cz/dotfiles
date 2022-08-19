13b0e211dfc09b44ccf6d0ba87fdcff15b3ff298
Antonin Fischer <tonda.fischer@gmail.com>
2022-08-19 14:50:59 +0200

    Improve pull all repos script.

--- bin/pull-all-repos-update.sh
+++ bin/pull-all-repos-update.sh
@@ -3,8 +3,8 @@ function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
-config pull
-cd ~/org && git pull
-cd ~/.password-store && pass git pull
-cd ~/Pictures/wallpapers/public-wallpapers && pass git pull
+config pull --autostash
+cd ~/org && git pull --autostash
+cd ~/.password-store && pass git pull --autostash
+cd ~/Pictures/wallpapers/public-wallpapers && pass git pull --autostash
 
