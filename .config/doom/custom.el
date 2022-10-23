b9115c7824f6a15dbe3b8b06f9d9f2d35e43305c
Antonin Fischer <tonda.fischer@gmail.com>
2022-10-23 14:27:05 +0200

    Small correction.

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/Documents/org-basics.org")))
+ '(warning-suppress-log-types '((initialization))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
