80fa4f300208d6668bd4688e3ec17756601d5fea
Antonin Fischer <tonda.fischer@gmail.com>
2022-10-27 13:47:05 +0200

    Squashed '.config/nvim/' changes from 78572ba2..76b6dd87
    76b6dd87 Usage of default_capabilities().
    6e6895c3 Use new toggleterm plugin.
    
    git-subtree-dir: .config/nvim
    git-subtree-split: 76b6dd87534ef0c8c5eb8cadbd9166aa07771f1d

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -88,13 +88,11 @@ M.on_attach = function(client, bufnr)
   lsp_highlight_document(client)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
