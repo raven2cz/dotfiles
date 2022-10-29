9c695337c791ab33502f5a7096683d8a62cc5d23
Antonin Fischer <tonda.fischer@gmail.com>
2022-10-29 11:33:47 +0200

    Refactor the script for aurupdates notification.

--- .root/etc/pacman.d/hooks.bin/aurupdates-notify.sh
+++ .root/etc/pacman.d/hooks.bin/aurupdates-notify.sh
@@ -3,12 +3,11 @@ uid="$(find /run/user/ -maxdepth 1 -mindepth 1 -type d | awk -F/ '{print $NF}')"
 user="$(grep $uid /etc/passwd  | sed 's/:.*//')" 
 display="$(DISPLAY=$(echo "$DISPLAY"))"
 xdg_rd="$(XDG_RUNTIME_DIR=/run/user/$uid)"
-xauth="$(XAUTHORITY=/home/$(grep $uid /etc/passwd  | sed 's/:.*//')/.Xauthority)"
+xauth="$(XAUTHORITY=/home/$user/.Xauthority)"
 dbus_sba="$(DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus)"
 
 export XAUTHORITY=/home/$user/.Xauthority
 export DISPLAY=$(echo "$DISPLAY")
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$uid/bus"
 
-su $(grep $uid /etc/passwd  | sed 's/:.*//') -c "DISPLAY=$(echo "$DISPLAY") $(XAUTHORITY=/home/$(grep $uid /etc/passwd  | sed 's/:.*//')/.Xauthority) DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus XDG_RUNTIME_DIR=/run/user/$uid notify-send 'Arch System is up-to-date!' && aurupdates-notify.sh"
-
+su $user -c "$display $xauth $dbus_sba $xdg_rd notify-send 'Arch System is up-to-date!' && aurupdates-notify.sh"
