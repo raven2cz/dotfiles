35e66131834d6185313ee654c26d707f051bc8a1
Antonin Fischer <tonda.fischer@gmail.com>
2022-11-24 09:05:12 +0100

    Add Wezterm configuration.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+return {
+    font = wezterm.font 'OperatorMono-Book',
+    color_scheme = 'MonokaiPro (Gogh)',
+    hide_tab_bar_if_only_one_tab = true,
+    window_close_confirmation = "NeverPrompt",
+    font_size = 12,
+	line_height = 0.95,
+	harfbuzz_features = { "calt=0" },
+    default_cursor_style = 'SteadyBlock',
+    window_background_opacity = 0.95,
+}
