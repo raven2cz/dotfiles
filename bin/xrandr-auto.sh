6f22da5fa5857369e0a7cd788754810260ef33e0
Antonin Fischer <antonin.fischer@firma.seznam.cz>
2022-12-08 21:50:12 +0100

    DPI modification according to displays.

--- bin/xrandr-auto.sh
+++ bin/xrandr-auto.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if xrandr | grep -q 'HDMI-1 connected' ; then
-    MONITOR=HDMI-1
-    xrandr --output eDP-1 --off --output $MONITOR --primary --dpi 120 --mode 3840x2160 --pos 0x0 --rotate normal 
-elif xrandr | grep -q 'DP-1 connected' ; then
+dp1_connected=$(xrandr --listactivemonitors | grep ' DP-1')
+hdmi1_connected=$(xrandr --listactivemonitors | grep ' HDMI-1')
+
+if [[ $dp1_connected != "" ]] ; then
     MONITOR=DP-1
+    xrandr --output eDP-1 --off --output $MONITOR --primary --dpi 120 --mode 3840x2160 --pos 0x0 --rotate normal 
+elif [[ $hdmi1_connected != "" ]] ; then
+    MONITOR=HDMI-1
     xrandr --output eDP-1 --off --output $MONITOR --primary --dpi 120 --mode 3840x2160 --pos 0x0 --rotate normal 
 else
     xrandr --output eDP-1 --dpi 100 --rotate normal 
