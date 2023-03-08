150c01185001fdb83fad08ed113393e401cc5407
Antonin Fischer <tonda.fischer@gmail.com>
2023-03-08 19:42:55 +0100

    New doom emacs location and settings.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -257,11 +257,11 @@ alias .5='cd ../../../../..'
 alias vim='nvim'
 alias em='/usr/bin/emacs -nw'
 alias emacs="emacsclient -c -a 'emacs'"
-alias doomsync="~/.emacs.d/bin/doom sync"
-alias doomdoctor="~/.emacs.d/bin/doom doctor"
-alias doomupgrade="~/.emacs.d/bin/doom upgrade"
-alias doompurge="~/.emacs.d/bin/doom purge"
-alias doomrun="~/.emacs.d/bin/doom run"
+alias doomsync="~/.config/emacs/bin/doom sync"
+alias doomdoctor="~/.config/emacs/bin/doom doctor"
+alias doomupgrade="~/.config/emacs/bin/doom upgrade"
+alias doompurge="~/.config/emacs/bin/doom purge"
+alias doomrun="~/.config/emacs/bin/doom run"
 
 # bat
 # alias cat='bat'
