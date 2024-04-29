8068857fdad4e0dad2560fb4d5b6968dedacf9d3
Antonin Fischer <tonda.fischer@gmail.com>
2024-04-29 09:21:36 +0200

    Start script for obsidian with xdg-open patch.

--- bin/obsidian-start.sh
+++ bin/obsidian-start.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Spustit Obsidian
-obsidian &
+# This script checks if Obsidian is already running to avoid starting multiple instances.
+# If not running, it launches Obsidian, waits for it to fully load, and then sets the default web browser back to Firefox Developer Edition.
+# This is necessary because Obsidian has a bug that sets itself as the default browser on launch.
 
-# ID procesu posledního pozadového úkolu
-PID=$!
+# Check if Obsidian is already running
+if pgrep -x "obsidian" > /dev/null; then
+    echo "Obsidian is already running."
+else
+    # Start Obsidian if not running
+    obsidian &
+    
+    # Get the process ID of the last background job
+    PID=$!
 
-# Počkáme, až se objeví proces Obsidianu v seznamu procesů
-while ! pgrep -x "obsidian" > /dev/null; do
-    sleep 1
-done
+    # Wait until Obsidian appears in the process list
+    while ! pgrep -x "obsidian" > /dev/null; do
+        sleep 1
+    done
 
-# Počkáme dodatečné 2 sekundy na zajištění, že je aplikace plně načtena
-sleep 2
+    # Additional wait to ensure Obsidian is fully loaded
+    sleep 2
 
-# Nastavení výchozího webového prohlížeče
-xdg-settings set default-web-browser firefoxdeveloperedition.desktop
+    # Set the default web browser to Firefox Developer Edition, addressing the issue of Obsidian changing the default browser on startup
+    xdg-settings set default-web-browser firefoxdeveloperedition.desktop
+fi
 
