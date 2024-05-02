ad4c1ac37424a572a842711b91712146ca3d8755
Antonin Fischer <tonda.fischer@gmail.com>
2024-05-02 09:19:03 +0200

    Obsidian start script updated.

--- bin/obsidian-start.sh
+++ bin/obsidian-start.sh
@@ -4,8 +4,8 @@
 # If not running, it launches Obsidian, waits for it to fully load, and then sets the default web browser back to Firefox Developer Edition.
 # This is necessary because Obsidian has a bug that sets itself as the default browser on launch.
 
-# Check if Obsidian is already running
-if pgrep -x "obsidian" > /dev/null; then
+# Check if Obsidian (electron) is already running using a specific path identification to avoid confusion with other electron applications
+if pgrep -f "/usr/lib/obsidian" > /dev/null; then    
     echo "Obsidian is already running."
 else
     # Start Obsidian if not running
