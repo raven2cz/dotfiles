285e824964aae799406c27f42e56e5cfdf5f82c7
Antonin Fischer <tonda.fischer@gmail.com>
2025-05-13 09:00:12 +0200

    New syntax pro awk and bash.

--- bin/aurupdates.sh
+++ bin/aurupdates.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-echo `checkupdates-with-aur | awk 'END { print (NR == 0 ? "Up to date" : NR " pkg" (NR > 1 ? "s" : "")); }'`
+#!/usr/bin/env bash
+echo "$(checkupdates-with-aur | awk 'END { print (NR==0 ? "Up to date" : NR " pkg" (NR>1?"s":"") ) }')"
